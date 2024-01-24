5f6ef869e34e0e9ae5cc94eff3b1f595450b0747
Drakonz <darkprince.selvan@gmail.com>
2024-01-24 15:51:39 +0530

    added char detail api point

--- server/server.js
+++ server/server.js
@@ -10,12 +10,50 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/char', async (req, res) => {
+const cache = {};
+
+//caching
+
+const cacheMiddleWare = (req, res, next) => {
+  const key = req.ogURL;
+  if (cache[key]) {
+    res.json(cache[key]);
+  } else {
+    next();
+  }
+};
+
+app.get('/api/char', cacheMiddleWare, async (req, res) => {
   const client = await db.connect();
 
   try {
     const result = await client.query('SELECT * FROM resident_evil_characters');
-    res.json(result.rows);
+    const characters = result.rows;
+
+    cache[req.originalUrl] = characters;
+    res.json(characters);
+  } catch (error) {
+    console.error('Error excuting query', error.stack);
+    res.status(500).json({ error: 'Something went wrong' });
+  } finally {
+    client.release();
+  }
+});
+
+app.get('/api/char/:id', async (req, res) => {
+  const { id } = req.params;
+  const client = await db.connect();
+
+  try {
+    const result = await client.query(
+      'SELECT * FROM resident_evil_characters WHERE id = $1',
+      [id]
+    );
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: 'Character not found' });
+    } else {
+      res.json(result.rows[0]);
+    }
   } catch (error) {
     console.error('Error excuting query', error.stack);
     res.status(500).json({ error: 'Something went wrong' });
