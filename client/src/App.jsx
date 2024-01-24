69b70a5a451ecc6215a6c48e40c428757fc62295
Drakonz <darkprince.selvan@gmail.com>
2024-01-24 16:49:41 +0530

    adding basic styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,19 @@ import './App.css';
 
 function App() {
   const [list, setList] = useState([]);
+  const [selectChar, setSelectChar] = useState(null);
+
+  const handleSelect = async (id) => {
+    try {
+      const result = await fetch(`http://localhost:3000/api/char/${id}`);
+      const charData = await result.json();
+      setSelectChar(charData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(list);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -17,11 +30,26 @@ function App() {
   }, []);
   return (
     <>
-      <div>
+      <main className="card-container">
         {list.map((item) => {
-          return <div key={item.id}>{item.name}</div>;
+          return (
+            <div
+              key={item.id}
+              className="card-item"
+              onClick={() => handleSelect(item.id)}
+            >
+              <h3>{item.name}</h3>
+              <p>{item.first_appearance}</p>
+            </div>
+          );
         })}
-      </div>
+        {selectChar && (
+          <div className="selected-char-container">
+            <h2>{selectChar.name}</h2>
+            <p>{selectChar.skills}</p>
+          </div>
+        )}
+      </main>
     </>
   );
 }
